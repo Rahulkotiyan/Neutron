66f3aaf4e531711e921f7bfdfae9817104675c1a
Rahulkotiyan <143371571+Rahulkotiyan@users.noreply.github.com>
2026-07-01 13:10:03 +0530

    splitting of vendor chunk implemented

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,10 +10,23 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      // This tells Vite: "When you see 'scenes', look in 'src/scenes'"
       scenes: path.resolve(__dirname, "./src/scenes"),
       state: path.resolve(__dirname, "./src/state"),
       components: path.resolve(__dirname, "./src/components"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/react-dom") || id.includes("node_modules/react/") || id.includes("node_modules/react-router") || id.includes("node_modules/axios")) return "vendor";
+          if (id.includes("node_modules/firebase")) return "firebase";
+          if (id.includes("node_modules/iconoir-react")) return "icons";
+          if (id.includes("node_modules/react-toastify")) return "ui";
+          if (id.includes("node_modules/posthog-js")) return "analytics";
+          if (id.includes("node_modules/socket.io-client")) return "socket";
+        },
+      },
     },
   },
 });
